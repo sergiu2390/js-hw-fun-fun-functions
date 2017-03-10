76f282654ba801a0f8cd5877b18a61a5d67d66e6
Christine Yi <christineyi@Christines-MacBook-Pro.local>
2017-03-10 10:52:06 -0500

    pushing master

---
+++ script.js
@@ -0,0 +1,70 @@
+// Question 1
+// 1.
+// 2.
+// 3.
+
+// Question 2
+// 1.
+// 2.
+// 3.
+
+// Question 3
+// 1.
+// 2.
+// 3.
+// 4.
+// 5.
+
+// Question 4
+// 1.
+// 2.
+// 3.
+// 4.
+// 5.
+
+// Question 5
+// 1.
+// 2.
+// 3.
+// 4.
+// 5.
+// 6.
+// 7.
+
+// Question 6
+
+var sumOfNums = function(numsArray){
+  // Your Code Here
+};
+
+// Question 7
+
+var numsGreaterThanTen = function(numsArray){
+  // Your Code Here
+};
+
+// Question 8
+
+var noneHaveTwoOrMoreVowels = function(words){
+  // Your Code Here
+};
+
+// Question 9
+var hasAtLeastNVowels = function(word, n){
+  // Your Code Here
+};
+
+// Question 10
+var buildObjectFromWords = function(words){
+  // Your Code Here
+};
+
+
+/// DO NOT EDIT BELOW THIS LINE ///
+module.exports = {
+  sumOfNums: sumOfNums,
+  numsGreaterThanTen: numsGreaterThanTen,
+  noneHaveTwoOrMoreVowels: noneHaveTwoOrMoreVowels,
+  buildObjectFromWords: buildObjectFromWords,
+  allStartingWithA: allStartingWithA
+}
